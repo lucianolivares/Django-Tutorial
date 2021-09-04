1babc7daae88361702735c2204cfd8de310464a3
lucianolivares <lucianolivares.lo@gmail.com>
2021-09-04 17:17:15 -0400

    add urls media

--- core/urls.py
+++ core/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('network.urls', namespace='network')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
